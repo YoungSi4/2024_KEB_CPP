34e3566ed2b28186c0c3efa151b147a8357fe4dd
YoungSi4 <dksdudgns01@gmail.com>
2024-01-23 16:03:17 +0900

    day01 - fin

--- KEB Cpp Note/day01.cpp
+++ KEB Cpp Note/day01.cpp
@@ -3,12 +3,41 @@
 using namespace std;
 
 int main() {
-	int ia = 0;
-	int a = 0;
-	const double Pi = 3.14;
+	/*
+	// 정수 정의
+	const unsigned int pennyValue = 1;
+	const unsigned int nickelValue = 5;
+	const unsigned int dimeValue = 10;
+	const unsigned int quarterValue = 25;
+	const unsigned int dollarValue = 100;
 
-	cout << "ia, a 값을 입력: ";
-	cin >> ia >> a;
-	cout << "ia * a = " << ia * a << endl;
-	cout << "Pi: " << Pi;
+	// 변수 정의 ( 각 코인 갯수)
+	unsigned int pennies;
+	unsigned int nickels;
+	unsigned int dimes;
+	unsigned int quarters;
+	unsigned int dollars;
+	// 전체 값을 나타내는 변수 선언
+	unsigned long totalValue;
+	//코인 입력받기
+	cout << "페니의 수: ";
+	cin >> pennies;
+	cout << "니켈의 수: ";
+	cin >> nickels;
+	cout << "다임의 수: ";
+	cin >> dimes;
+	cout << "쿼터의 수";
+	cin >> quarters;
+	cout << "달러의 수";
+	cin >> dollars;
+	*/
+	/*
+	cout << "Hello";
+	cout << "Hello\n";
+	cout << "Hello\b";
+	*/
+
+	// \' , \" 있는 이유.
+	// cout >> ""hi"";
+	return 0;
 }
