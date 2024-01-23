f4e9cd43ad22b39603155cd26c33eb5cc4de0230
YoungSi4 <dksdudgns01@gmail.com>
2024-01-23 14:46:50 +0900

    cout cin

--- KEB Cpp Note/day01.cpp
+++ KEB Cpp Note/day01.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl << "Cpp opertating.";
+	cout << "Hello, World!" << endl << "Cpp opertating." << endl;
+	cout << "*****" << endl << "*****" << endl << "*****" << endl << "*****" << endl;
+	int number1 = 0;
+	int number2 = 0;
+	cout << "정수를 두 개 입력: ";
+	cin >>  number1 >> number2;
+	cout << "두 숫자의 합: " << number1 + number2;
 	return 0;
 }
+
